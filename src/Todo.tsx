9d6e11b3601676c5d1a374285b01bfacb93835fb
kubo-work <kubowork0501@gmail.com>
2024-06-17 21:11:06 +0900

    todoアプリtsx化

---
+++ src/Todo.tsx
@@ -0,0 +1,70 @@
+import React,{ useState } from "react";
+import "./App.css";
+import { InputTodos } from "./components/InputTodo";
+import { IncompleteTodos } from "./components/IncompleteTodos";
+import { CompleteTodos } from "./components/CompleteTodos";
+
+export const Todo:React.FC = () => {
+  const [todoText, setTodoText] = useState<string>("");
+  const [incompleteTodos, setIncompleteTodos] = useState<string[]>([]);
+  const [completeTodos, setCompleteTodos] = useState<string[]>([]);
+
+  const onChangeTodoText = (event:React.ChangeEvent<HTMLInputElement>) => {
+    setTodoText(event.target.value);
+  };
+
+  const onClickAdd = () => {
+    if (todoText === "") return;
+    const newTodos:string[] = [...incompleteTodos, todoText];
+    setIncompleteTodos(newTodos);
+    setTodoText("");
+  };
+
+  const onClickDelete = (index:number) => {
+    const newTodos = [...incompleteTodos];
+    newTodos.splice(index, 1);
+    setIncompleteTodos(newTodos);
+  };
+
+  const onClickComplete = (index:number) => {
+    const newInCompleteTodos = [...incompleteTodos];
+    newInCompleteTodos.splice(index, 1);
+
+    const newCompleteTodos = [...completeTodos, incompleteTodos[index]];
+
+    setIncompleteTodos(newInCompleteTodos);
+    setCompleteTodos(newCompleteTodos);
+  };
+
+  const onClickBack = (index:number) => {
+    const newCompleteTodos = [...completeTodos];
+    newCompleteTodos.splice(index, 1);
+
+    const newInCompleteTodos = [...incompleteTodos, completeTodos[index]];
+
+    setCompleteTodos(newCompleteTodos);
+    setIncompleteTodos(newInCompleteTodos);
+  };
+
+  const isMaxLimitIncompleteTodos:number = 5;
+
+  return (
+    <>
+      <InputTodos
+        todoText={todoText}
+        onChange={onChangeTodoText}
+        onClick={onClickAdd}
+        disabled={incompleteTodos.length >= isMaxLimitIncompleteTodos}
+      />
+      {incompleteTodos.length >= isMaxLimitIncompleteTodos && (
+        <p style={{ color: "red" }}>登録できるTODOは5個まで</p>
+      )}
+      <IncompleteTodos
+        todos={incompleteTodos}
+        onClickComplete={onClickComplete}
+        onClickDelete={onClickDelete}
+      />
+      <CompleteTodos todos={completeTodos} onClickBack={onClickBack} />
+    </>
+  );
+};
---
+++ src/components/InputTodo.tsx
@@ -0,0 +1,25 @@
+const style = {
+    background: "#c6e5d9",
+    width: "400px",
+    height: "30px",
+    padding: "8px",
+    margin: "8px",
+    borderRadius: "8px"
+}
+
+type Props = {
+    todoText:string,
+    onChange:(event:React.ChangeEvent<HTMLInputElement>) => void,
+    onClick:() => void,
+    disabled:boolean
+}
+
+export const InputTodos = (props:Props) => {
+    const {todoText,onChange,onClick,disabled } = props
+    return(
+        <div style={style}>
+            <input disabled={disabled} placeholder='TODOを入力' value={todoText} onChange={onChange}/>
+            <button disabled={disabled} onClick={onClick}>追加</button>
+      </div>
+    )
+}
